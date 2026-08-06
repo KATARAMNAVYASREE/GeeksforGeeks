10e716b8c0e34899c7ad0165b78c7c418b72b30f
Navya Sree <kataramnavyasree05@gmail.com>
2026-08-06 12:14:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Zero Sum Subarrays/zero-sum-subarrays.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findSubarray(self, arr):
+        prefix_sum = 0
+        count = 0
+        freq = {0: 1}  
+
+        for num in arr:
+            prefix_sum += num
+
+            if prefix_sum in freq:
+                count += freq[prefix_sum]
+            freq[prefix_sum] = freq.get(prefix_sum,0)+1
+        return count
